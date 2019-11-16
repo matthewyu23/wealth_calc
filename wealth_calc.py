40fc1f1841e9fb47cbbea90a23c0db05ad1974fd
Matthew Yu <matthew@Matthews-MacBook-Pro.local>
2019-11-15 18:25:52 -0800

    basic functionalities of predicting, still need clean up

--- wealth_calc.py
+++ wealth_calc.py
@@ -10,3 +10,25 @@ def calc_tax(pre_tax_income):
             tax += bracket_percent[i] * bracket_income[i]
             pre_tax_income -= bracket_income[i]
     return bracket_percent[-1] * pre_tax_income + tax
+
+def standardize_length(s, l): 
+    if not isinstance(s, str): 
+        s = str(s)
+    for x in range(l - len(s)): 
+        s += " "
+    return s
+
+class Person: 
+    def __init__(self, age, income, needs_ratio, wants_ratio, savings_ratio): 
+        self.income, self.age, self.needs_ratio, self.wants_ratio, self.savings_ratio = income, age, needs_ratio, wants_ratio, savings_ratio
+        self.column_width = 20
+        self.savings = 0
+
+
+    def predict(self, years): 
+        print(standardize_length("age", self.column_width), standardize_length("income", self.column_width), standardize_length("income after tax", self.column_width), standardize_length("savings", self.column_width))
+        for x in range(self.age, self.age + years + 1): 
+            income_after_tax = self.income-calc_tax(self.income)
+            savings_from_this_year = self.savings_ratio/100*income_after_tax
+            self.savings += savings_from_this_year
+            print(standardize_length(x, self.column_width), standardize_length(self.income, self.column_width), standardize_length(income_after_tax, self.column_width), standardize_length((self.savings), self.column_width))
